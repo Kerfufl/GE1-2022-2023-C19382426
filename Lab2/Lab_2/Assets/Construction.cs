f67118a77063b00b1c8db30637ceec851760fcc2
Kerfufl <c19382426@mytudublin.ie>
2022-10-01 00:17:26 +0100

    Constructor tweak
    Tweaked object instantiation

--- Lab2/Lab_2/Assets/Construction.cs
+++ Lab2/Lab_2/Assets/Construction.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Construction : MonoBehaviour
 {
     public GameObject cubepref;
+    GameObject bruh;
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(cubepref, gameObject.transform.position,Quaternion.identity);
+        bruh = Instantiate(cubepref, gameObject.transform.position,Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bruh.transform.Rotate(0,.09f,0.09f);
     }
 }
