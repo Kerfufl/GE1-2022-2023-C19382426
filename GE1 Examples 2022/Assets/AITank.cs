7dcd8b9fe848c0c221334de6c4f9900c3de2e68d
Kerfufl <c19382426@mytudublin.ie>
2022-10-19 23:40:38 +0100

    Gizmo Exploration
    Tried getting my head around the gizmos, didn't work out too well

--- GE1 Examples 2022/Assets/AITank.cs
+++ GE1 Examples 2022/Assets/AITank.cs
@@ -11,7 +11,7 @@ public class AITank : MonoBehaviour {
     List<Vector3> waypoints = new List<Vector3>();
     public float speed = 10;
     public Transform player;    
-
+    
     public void OnDrawGizmos()
     {
         if (!Application.isPlaying)
@@ -22,6 +22,10 @@ public class AITank : MonoBehaviour {
             // You can draw gizmos using
             // Gizmos.color = Color.green;
             // Gizmos.DrawWireSphere(pos, 1);
+
+            /*
+            Gizmos weren't very well explained in class or lab
+            */
         }
     }
 
@@ -44,6 +48,6 @@ public class AITank : MonoBehaviour {
         // Task 5
         // Put code here to calculate if the player is inside the field of view and in range
         // You can print stuff to the screen using:
-        GameManager.Log("Hello from th AI tank");
+        GameManager.Log("Hello from the AI tank");
     }
 }
